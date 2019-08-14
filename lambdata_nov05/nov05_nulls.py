72fe19d359a58055e3a92f24d10df3f30efcdaa7
Nathan Van Wyck <nathanvanwyck@Macintosh-00254bbd8f8a-2.local>
2019-08-14 11:34:45 -0400

    Formatted with pep8online and added docstring

--- lambdata_nov05/nov05_nulls.py
+++ lambdata_nov05/nov05_nulls.py
@@ -1,11 +1,10 @@
 def nov05_colnulls(df):
+    '''Takes pandas DataFrame and prints number of nulls for each column and
+    for whole DataFrame.'''
     colnulls = []
     for col in df:
         count = df[col].isnull().sum()
         colnulls.append(count)
         print("Column", col, "has", count, "null values.")
-        
     print('Data frame has', sum(colnulls), 'null values.')
     return colnulls
-
-
